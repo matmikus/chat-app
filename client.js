a9b37fb83521d60dcca6077ce224fd030d4d16ba
matmikus <mateusz.mikolajczak@yahoo.com>
2018-03-11 00:15:10 +0100

    modals

--- client.js
+++ client.js
@@ -4,3 +4,32 @@ function scrollToBottom(id){
 }
 scrollToBottom('log-box');
 scrollToBottom('chat-box');
+
+var statsModal = document.getElementById('stats-modal');
+var statsButton = document.getElementById('show-stats');
+var statsClose = document.getElementsByClassName('modal-close')[0];
+statsButton.onclick = function() {
+    statsModal.style.display = 'flex';
+}
+statsClose.onclick = function() {
+    statsModal.style.display = 'none';
+}
+window.onclick = function(event) {
+    if (event.target == statsModal) {
+        statsModal.style.display = 'none';
+    }
+}
+
+var loginModal = document.getElementById('login-modal');
+var enterButton = document.getElementById('enter');
+window.onload = function(event) {
+    loginModal.style.display = 'flex';
+}
+enterButton.onclick = function() {
+  loginModal.style.display = 'none';
+}
+
+var clearButton = document.getElementById('clear-log');
+clearButton.onclick = function() {
+  document.getElementById('log-box').innerHTML = "";
+}
